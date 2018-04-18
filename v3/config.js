09d472db792d34962a776052207de58991868bdd
Estuardo <amestuardo@gmail.com>
2018-04-18 10:53:12 -0600

    creacion archivo de configuracion creacion modelo datos.js

---
+++ v3/config.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'autoparking',
+        password: 'autoparking',
+        database: 'autoparking'
+    }
+)
+module.exports = connection;
--- v3/index.js
+++ v3/index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded ({extended:false}))
 app.use(bodyParser.json())
 
+const dato = require ('./models/dato')
 
 //llamamos al paquete mysql que hemos instalado
 const mysql = require('mysql'),
@@ -26,26 +27,47 @@ connection.connect()
 
 
 app.get('/api/dato',function (req,res)
-{
+{/*
   connection.query('SELECT * FROM autoparking.datos', function(error, results, fields)
 {
   if (error) throw error
   return res.send ({erro: false, data: results, message: 'Todos listen'})
 
 })
+*/
+dato.getAllDatos(function(err, rows) {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(rows);
+            }
+        });
+
+
 })
 
 app.get ('/api/dato/:datoId', (req,res) =>
 {
+  /*
 let datoId = req.params.datoId
 connection.query("SELECT * FROM datos WHERE id_datos = ?",datoId,function (error, results, fields) {
   if (error) throw error
   return res.send ({error: false, data: results[0], message: 'Search List' })
-})
+})*/
+
+dato.getDatoById (req.params.datoId, function(err, rows) {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(rows);
+            }
+        });
+
+
 })
 
 app.post ('/api/dato', (req,res) =>
-{
+{/*
   console.log(req.body)
   let id_datos = req.body.id_datos
   let Nombre = req.body.Nombre
@@ -61,21 +83,62 @@ app.post ('/api/dato', (req,res) =>
     connection.query ('INSERT INTO  datos (id_datos, Nombre, Apellido, Direccion, Correo) VALUES ("' +id_datos+ '","' +Nombre+ '","' +Apellido+ '","' +Direccion+ '","' +Correo+ '")', function (error, results, fields) {
       if(error) throw error
       return res.send({error: false, data: results, message: 'New task has been created successfully.'})
-})
+})*/
+
+dato.addDato(req.body, function(err, count) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(req.body); //or return count for 1 & 0
+        }
+    });
+
 })
 
-app.put ('/api/dato/:datoId', (req,res) =>
-{
+app.put ('/api/dato/:id_datos', (req,res) =>
+{/*
+let id_datos =req.params.id_datos
+let Nombre = req.body.Nombre
+let Apellido = req.body.Apellido
+let Direccion = req.body.Direccion
+let Correo = req.body.Correo
 
-} )
+if (!id_datos) {
+        return res.status(400).send({ error: id_datos, message: 'Please provide task and task_id' })
+              }
+
+connection.query("UPDATE datos SET Nombre=?,Apellido=?, Direccion=?, Correo=? WHERE id_datos=?" ,[Nombre, Apellido, Direccion, Correo, id_datos], function (error, results, fields) {
+ if (error) throw error
+ return res.send({ error: false, data: results, message: 'Task has been updated successfully.' })
+})*/
+
+dato.updateDato(req.params.id_datos, req.body, function(err, rows) {
+       if (err) {
+           res.json(err);
+       } else {
+           res.json(rows);
+       }
+   });
+
+})
 
 app.delete ('/api/dato/:datoId', (req,res) =>
 {
+  /*
   let datoId = req.params.datoId
   connection.query('DELETE FROM datos WHERE id_datos = ?', [datoId], function (error, results, fields) {
         if (error) throw error;
         return res.send({ error: false, data: results, message: 'se ha eliminado con exito' });
-    })
+    })*/
+
+    dato.deleteDato(req.params.datoId, function(err, count) {  
+           if (err) {
+               res.json(err);
+           } else {
+               res.json(count);
+           }
+       });
+
 } )
 
 app.listen(port,() =>
